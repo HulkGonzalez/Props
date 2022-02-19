874f3d86a71b1457ced67626d35be62527ae4975
Cesar Gonzalez <cesargonzalezdc@gmail.com>
2022-02-18 21:48:34 -0300

    Estructura Vue con props

---
+++ src/App.vue
@@ -0,0 +1,62 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <h1>Agregar tareas</h1>
+    <form v-on:submit.prevent="agregarTarea">
+      <h3>Tarea: <input type="text" v-model="nombre" /></h3>
+      <button type="submit">  Añade  </button>
+    </form>
+    </div>
+    <h2>Lista</h2>
+
+    <HelloWorld :tareas="this.tareas" @deleteItem="deleteItem"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+export default {
+  name: "App",
+  data() {
+    return {
+      nombre: "",
+      tareas: [],
+    };
+  },
+  methods: {
+    agregarTarea: function () {
+      this.tareas.push({
+        nombre: this.nombre,
+      })
+      this.nombre = ""
+    },
+    deleteItem(index){
+      this.tareas.splice(index, 1)
+    }
+  },
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+input{
+  width: 8rem;
+  margin-right: 20px;
+}
+
+body{
+  display: flex;
+  justify-content: center;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="hello">
+   <ul v-for="(tarea, index) in tareas" :key="tarea">
+      <li><span>{{ index+1 }} - {{ tarea.nombre }}</span>  <button @click="deleteItem(index)">Eliminar</button></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    tareas: Array
+  },
+  methods:{
+    deleteItem(index){
+      this.$emit("deleteItem", index)
+    }
+  }
+}
+</script>
+
+<style scoped>
+  ul{
+    display: flex;
+    justify-content: center;
+  }
+  span{
+    font-size: medium;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    margin-left: 1rem;
+  }
+  button{
+    width: 5rem;
+    height: 2rem;
+    background: red;
+    color: #fff;
+    border-radius: 10%;
+    margin-left: 1rem;
+    border: none;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    text-transform: uppercase;
+  }
+</style>
